9d5e08720ac6e17d7a5a196540c284618b53c20e
Tony Valderrama <tony.valderrama@gmail.com>
2025-12-17 13:37:26 -0600

    Add files via upload

--- src/test/java/com/epam/tat/module7/config/ConfigURLs.java
+++ src/test/java/com/epam/tat/module7/config/ConfigURLs.java
@@ -7,4 +7,5 @@ public abstract class ConfigURLs {
     public static final String LOGIN_URL = "https://the-internet.herokuapp.com/login";
     public static final String SECURE_AREA_URL = "https://the-internet.herokuapp.com/secure";
     public static final String MULTIPLEWINDOWS_URL = "https://the-internet.herokuapp.com/windows";
+    public static final String CANDYWRAPPER_URL = "https://candymapperr2.com/launch-candymapper";
 }
